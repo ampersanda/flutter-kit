62348965e43449ae88b80c5435889017ded708c9
Mochamad Lucky Pradana <luckypradana123@yandex.ua>
2019-09-06 00:01:08 +0700

    set target sdk version

--- src/flutter_kit/androidx.clj
+++ src/flutter_kit/androidx.clj
@@ -6,5 +6,11 @@
   (write-file app-gradle-path
               (clojure.string/replace-first (slurp app-gradle-path) #"compileSdkVersion\s*(\d+)" (str "compileSdkVersion " version))))
 
+(defn- set-target-sdk-version [version]
+  "change targetSdkVersion"
+  (write-file app-gradle-path
+              (clojure.string/replace-first (slurp app-gradle-path) #"targetSdkVersion\s*(\d+)" (str "targetSdkVersion " version))))
+
 (defn install! []
-  (set-compile-sdk-version 28))
+  (set-compile-sdk-version 28)
+  (set-target-sdk-version 28))
